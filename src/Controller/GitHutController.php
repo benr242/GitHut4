b27f0999ce36ccade8e434b4c04fc6373cba61cd
Benjamin Rose <benr242@localhost.localdomain>
2018-04-24 12:10:24 -0400

    check

--- src/Controller/GitHutController.php
+++ src/Controller/GitHutController.php
@@ -12,23 +12,29 @@ namespace App\Controller;
 use App\Service\GitHubApi;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
-class GitHutController extends Controller
+class GitHutController extends AbstractController
 {
     private $username;
 
     /**
-     * @Route("/{username}", name="default", defaults={"username"="benr242"})
+     * @Route("/user/{username}", name="user", defaults={"username"="benr242"})
      */
     public function githutAction(Request $request, GitHubApi $api, $username, LoggerInterface $logger)
     {
         dump($username);
         $logger->info(json_encode('username::GitHut: ' . $username));
 
+        $userdata = $api->getProfile($username);
+        $userdata2 = $api->getRepos($username);
+
+
         return $this->render('githut/index.html.twig', [
-            'username' => $username
+            'username' => $username,
+            'userdata' => $userdata,
+            'userdata2' => $userdata2
         ]);
 
     }
